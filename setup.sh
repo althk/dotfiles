bb3b36182fcd6c1af8e8e7582d3a4ffa259fa3dc
Harish Kotha <althk@users.noreply.github.com>
2018-06-10 11:46:18 -0700

    fix minor issue where dirname returns '.' if setup.sh is run from current dir.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 set -x
 
 DOTFILES_DIR="$(dirname $0)"
+# dirname resolves to '.' instead of the full path
+# if the script is run from the CWD.
+if [[ "${DOTFILES_DIR}" = "." ]]; then
+	DOTFILES_DIR="${PWD}"
+fi
+echo "DOTFILES_DIR: $DOTFILES_DIR"
 export DOTFILES_DIR
 
 lh() {
