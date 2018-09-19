8eb2cae705ddec3225c4b3d99a0ccbba71c6d3ce
Harish Kotha <althk@users.noreply.github.com>
2018-09-18 22:57:32 -0700

    update golang to 1.11

--- install/golang.sh
+++ install/golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GO_VERSION="go1.10.3"  # Figure out how to keep this updated.
+GO_VERSION="go1.11"  # Figure out how to keep this updated.
 GO_DL_NAME="$GO_VERSION.linux-amd64.tar.gz"
 
 echo "Installing golang ($GO_VERSION)"
