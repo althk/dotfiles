400f5a3baa7325c4d0b3f0ae7e4962601748db3d
Harish Kotha <althk@users.noreply.github.com>
2021-09-01 21:28:41 +0530

    adding docker installation script to the repo; note: not installing docker on setup

---
+++ install/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl -fsSL https://download.docker.com/linux/debian/gpg \
+	| sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Replace bullseye with $(lsb_release -cs) when the repo is ready for the new testing version.
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
