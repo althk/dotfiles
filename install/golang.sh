c5de22409a97a672fb1ac9c6cfa8d16c2baea8c1
Harish Kotha <althk@users.noreply.github.com>
2023-02-15 11:44:13 +0530

    update golang version to 1.20.1; also delete existing version before extracting the new one

--- install/golang.sh
+++ install/golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GO_VERSION="go1.19.3"  # Figure out how to keep this updated.
+GO_VERSION="go1.20.1"  # Figure out how to keep this updated.
 GO_DL_NAME="$GO_VERSION.linux-amd64.tar.gz"
 
 export GOPATH="$HOME/.go"
@@ -15,6 +15,7 @@ wget https://dl.google.com/go/"$GO_DL_NAME" -O "/tmp/$GO_DL_NAME"
 
 
 [ -d $GO_INSTALL_DIR ] || mkdir -p $GO_INSTALL_DIR
+[ -d $GO_INSTALL_DIR/go ] && rm -rf $GO_INSTALL_DIR/go 
 tar -zxf "/tmp/$GO_DL_NAME" -C $GO_INSTALL_DIR
 
 
