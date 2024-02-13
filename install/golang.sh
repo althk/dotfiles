097b349c28e7df4843360569fc25c67f99370c35
Harish Kotha <althk@users.noreply.github.com>
2024-02-13 23:24:30 +0530

    update golang ver to 1.22.0

--- install/golang.sh
+++ install/golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GO_VERSION="go1.20.1"  # Figure out how to keep this updated.
+GO_VERSION="go1.22.0"  # Figure out how to keep this updated.
 GO_DL_NAME="$GO_VERSION.linux-amd64.tar.gz"
 
 export GOPATH="$HOME/.go"
